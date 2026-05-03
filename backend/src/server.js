6593f8e3466e40f1e726ff35f278e545cf4f4c78
Tanay Bansal <tanaybansal16@gmail.com>
2026-05-03 19:30:50 +0530

    fix production config

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const PORT = ENV.PORT || 3000;
 app.use(express.json({ limit: "5mb" })); // req.body
 app.use(
   cors({
-    origin: "https://chattify-chi.vercel.app",
+    origin: ["http://localhost:5173", "https://chattify-chi.vercel.app"],
     credentials: true,
   })
 );
